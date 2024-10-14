04805f6474928a98a4dbdeea1535f742ac9d4e40
Ken Seehart <ken@agi.green>
2024-10-13 17:20:05 -0700

    switched from venv to conda

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname, basename
 
-with open('requirements.txt') as f:
-    lines = f.read().splitlines()
-
-# Split the requirements and the editable (-e) dependencies
-requirements = [line for line in lines if not line.startswith("-e")]
-dependency_links = [line.split("-e ")[-1] for line in lines if line.startswith("-e")]
-
 setup(
     name=basename(dirname(__file__)),
     version='0.1',
     packages=find_packages(),
-    install_requires=requirements,
-    dependency_links=dependency_links
+    install_requires=[],  # Leave this empty as conda will handle dependencies
+    include_package_data=True,
+    # Add any other necessary setup parameters here
 )
