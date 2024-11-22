b7a16dfee573508c6e9bc5e4c13b32823050fef0
Ken Seehart <ken@evolver.ai>
2024-11-22 04:22:31 -0800

    ignore

--- agi_green/frontend/dist/assets/channel-BKfYN3Ui.js
+++
@@ -1,8 +0,0 @@
-import { aE as Utils, aF as Color } from "./index-TCZc8GNC.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
-//# sourceMappingURL=channel-BKfYN3Ui.js.map
